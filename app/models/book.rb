2b2289a83f645d15b07ea50117189ce6e139b2e7
chaprynz <henrysai89@gmail.com>
2016-04-10 09:37:49 +0000

    ready2

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ActiveRecord::Base
     has_many :bookposts    
-    validates :title, presence: true, length: { maximum: 50 }
-    validates :note, presence: true
-    validates :header, presence: true
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :content, presence: true
     validates :tag, presence: true
 end
